9fdef3fb0d683465ea5619b9c0ef524c4faadcb2
Elisjoshi <98356711+Elisjoshi@users.noreply.github.com>
2025-10-14 19:08:21 -0700

    Setup pages (#5)
    I am merging this myself because I want to and I can :)

---
+++ src/app/(tabs)/charities.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function charities() {
+  return (
+    <View style={styles.container}>
+      <Text>Charities</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
