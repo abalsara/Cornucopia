2a7129d93ebbf3e7d2b247a5fa8ec9c51a90a79c
Charles Bither <charles@charlesbither.com>
2025-10-25 10:11:37 -0700

    remove hello-world function

---
+++ supabase/migrations/20251025032040_add_service_role_permissions.sql
@@ -0,0 +1,69 @@
+grant delete on table "public"."Charities" to "service_role";
+
+grant insert on table "public"."Charities" to "service_role";
+
+grant references on table "public"."Charities" to "service_role";
+
+grant select on table "public"."Charities" to "service_role";
+
+grant trigger on table "public"."Charities" to "service_role";
+
+grant truncate on table "public"."Charities" to "service_role";
+
+grant update on table "public"."Charities" to "service_role";
+
+grant delete on table "public"."Donation" to "service_role";
+
+grant insert on table "public"."Donation" to "service_role";
+
+grant references on table "public"."Donation" to "service_role";
+
+grant select on table "public"."Donation" to "service_role";
+
+grant trigger on table "public"."Donation" to "service_role";
+
+grant truncate on table "public"."Donation" to "service_role";
+
+grant update on table "public"."Donation" to "service_role";
+
+grant delete on table "public"."Profiles" to "service_role";
+
+grant insert on table "public"."Profiles" to "service_role";
+
+grant references on table "public"."Profiles" to "service_role";
+
+grant select on table "public"."Profiles" to "service_role";
+
+grant trigger on table "public"."Profiles" to "service_role";
+
+grant truncate on table "public"."Profiles" to "service_role";
+
+grant update on table "public"."Profiles" to "service_role";
+
+grant delete on table "public"."Request" to "service_role";
+
+grant insert on table "public"."Request" to "service_role";
+
+grant references on table "public"."Request" to "service_role";
+
+grant select on table "public"."Request" to "service_role";
+
+grant trigger on table "public"."Request" to "service_role";
+
+grant truncate on table "public"."Request" to "service_role";
+
+grant update on table "public"."Request" to "service_role";
+
+grant delete on table "public"."RequestPost" to "service_role";
+
+grant insert on table "public"."RequestPost" to "service_role";
+
+grant references on table "public"."RequestPost" to "service_role";
+
+grant select on table "public"."RequestPost" to "service_role";
+
+grant trigger on table "public"."RequestPost" to "service_role";
+
+grant truncate on table "public"."RequestPost" to "service_role";
+
+grant update on table "public"."RequestPost" to "service_role";
