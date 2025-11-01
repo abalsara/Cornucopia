f6fd31c57531387fbb422efc981de8253a1f615a
Charles Bither <charles@charlesbither.com>
2025-11-01 11:12:32 -0700

    use Map instead of array to store saved donations state, so that duplicate DonationItems are not saved

--- src/stores/savedDonations.ts
+++ src/stores/savedDonations.ts
@@ -1,14 +1,16 @@
 import { DonationItem } from '../types/DonationItem/DonationItem.types';
 
-// store the user's saved donations
-let donations: DonationItem[] = [];
+/**
+ * Store the user's saved donations for a single charity during the schedule a donation user flow.
+ * Maps DonationItem.name to the corresponding DonationItem.
+ */
+let donations: Map<string, DonationItem> = new Map();
 
 /**
- *
  * @returns the user's saved donations
  */
 export const getSavedDonations = (): DonationItem[] => {
-  return [...donations];
+  return Array.from(donations.values());
 };
 
 /**
@@ -16,12 +18,12 @@ export const getSavedDonations = (): DonationItem[] => {
  * @param item - The DonationItem to store
  */
 export const saveDonation = (item: DonationItem): void => {
-  donations.push(item);
+  donations.set(item.itemName, item);
 };
 
 /**
  * Clears the user's saved donations
  */
 export const resetSavedDonations = (): void => {
-  donations = [];
+  donations = new Map();
 };
