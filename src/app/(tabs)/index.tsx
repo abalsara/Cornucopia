1446676ed4499bbd9ac8a8121e6b25c3839af40a
Charles Bither <charles@charlesbither.com>
2025-11-04 17:40:16 -0800

    update my donations tab to use list component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,11 +5,9 @@ import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator, Text } from 'react-native-paper';
 
 import ThemedView from '@/src/components/ThemedView';
-import Navbar from '@/src/components/bars/Navbar';
-import ScheduledDonationCard from '@/src/components/cards/ScheduledDonationCard';
+import ScheduledDonationCardList from '@/src/components/lists/ScheduledDonationCardList';
 import { supabase } from '@/src/lib/supabase';
 import 'react-native-url-polyfill/auto';
-import { getScheduledDonations } from '@/src/stores/scheduledDonations';
 
 export default function Index() {
   const [loading, setLoading] = useState(true);
@@ -42,19 +40,11 @@ export default function Index() {
     );
   }
 
-  const scheduledDonations = getScheduledDonations();
   return (
     <ThemedView>
-      <Navbar title="My Donations" backButtonShown={false} />
       <View style={styles.container}>
         <Text>Upcoming</Text>
-        {scheduledDonations.map((scheduledDonation) => (
-          <ScheduledDonationCard
-            cid={scheduledDonation.cid}
-            schedule={scheduledDonation.scheduledDate}
-            key={`${scheduledDonation.cid}${scheduledDonation.scheduledDate}`}
-          />
-        ))}
+        <ScheduledDonationCardList />
       </View>
     </ThemedView>
   );
